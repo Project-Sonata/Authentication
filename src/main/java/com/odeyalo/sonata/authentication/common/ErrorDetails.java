74ed8fcf2f2ba30ae1e4544ab18866ade73b5899
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-27 23:09:10 +0200

    Contains info about error

---
+++ src/main/java/com/odeyalo/sonata/authentication/common/ErrorDetails.java
@@ -0,0 +1,28 @@
+package com.odeyalo.sonata.authentication.common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains info about error
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorDetails {
+    public static final ErrorDetails INVALID_EMAIL = of("invalid_email", "The provided email has invalid format", "To solve the problem try to enter valid email with valid pattern");
+    public static final ErrorDetails EMAIL_ALREADY_TAKEN = of("email_already_taken", "The provided email already in use by other user", "To solve the problem try to enter other email");
+    public static final ErrorDetails INVALID_PASSWORD = of("invalid_password", "The password is invalid, password must contain at least 8 characters and 1 number", "To fix the problem - input the correct password with required format");
+
+    private String code;
+    private String description;
+    @JsonProperty("possible_solution")
+    private String possibleSolution;
+
+    public static ErrorDetails of(String code, String description, String possibleSolution) {
+        return new ErrorDetails(code, description, possibleSolution);
+    }
+}

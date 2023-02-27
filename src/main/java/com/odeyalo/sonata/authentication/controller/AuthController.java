b1d38e32164eefbc65560bf10811080507f8e779
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-27 23:06:33 +0200

    Replaced hard-coded validations with UserRegistrationInfoValidator

--- src/main/java/com/odeyalo/sonata/authentication/controller/AuthController.java
+++ src/main/java/com/odeyalo/sonata/authentication/controller/AuthController.java
@@ -3,7 +3,8 @@ package com.odeyalo.sonata.authentication.controller;
 import com.odeyalo.sonata.authentication.dto.error.ApiErrorDetailsInfo;
 import com.odeyalo.sonata.authentication.dto.response.UserRegistrationConfirmationResponseDto;
 import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
-import org.apache.commons.validator.routines.EmailValidator;
+import com.odeyalo.sonata.authentication.support.validation.UserRegistrationInfoValidator;
+import com.odeyalo.sonata.authentication.support.validation.ValidationResult;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -15,28 +16,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.regex.Pattern;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserRegistrationInfoValidator userRegistrationInfoValidator;
+
+    public AuthController(UserRegistrationInfoValidator userRegistrationInfoValidator) {
+        this.userRegistrationInfoValidator = userRegistrationInfoValidator;
+    }
 
     @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> registerUser(@RequestBody UserRegistrationInfo info) {
         UserRegistrationConfirmationResponseDto dto = new UserRegistrationConfirmationResponseDto("We sent confirmation letter to your email. Check it out");
-        if (!EmailValidator.getInstance().isValid(info.getEmail())) {
-            return ResponseEntity.badRequest().body(new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, ApiErrorDetailsInfo.ErrorDetails.INVALID_EMAIL));
+        ValidationResult result = userRegistrationInfoValidator.validateInfo(info);
+        if (!result.isSuccess()) {
+            return ResponseEntity.badRequest().body(new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, result.getErrorDetails()));
         }
-
-        if (info.getEmail().equals("alreadytaken@gmail.com")) {
-            return ResponseEntity.badRequest().body(new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, ApiErrorDetailsInfo.ErrorDetails.EMAIL_ALREADY_TAKEN));
-        }
-
-        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$");
-        if (!pattern.matcher(info.getPassword()).matches()) {
-            return ResponseEntity.badRequest().body(new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, ApiErrorDetailsInfo.ErrorDetails.INVALID_PASSWORD));
-        }
-
         return getSuccessResponse(info, dto);
     }
 

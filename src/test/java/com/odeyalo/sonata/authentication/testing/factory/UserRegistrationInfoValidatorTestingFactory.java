9bc4b6eecb1bd9259bfd9576ff3962392b244fc2
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-28 15:14:06 +0200

    Renamed

---
+++ src/test/java/com/odeyalo/sonata/authentication/testing/factory/UserRegistrationInfoValidatorTestingFactory.java
@@ -0,0 +1,39 @@
+package com.odeyalo.sonata.authentication.testing.factory;
+
+import com.odeyalo.sonata.authentication.support.validation.DefaultChainUserRegistrationInfoValidator;
+import com.odeyalo.sonata.authentication.support.validation.UserRegistrationInfoValidationStepContainer;
+import com.odeyalo.sonata.authentication.support.validation.UserRegistrationInfoValidationStepRegistry;
+import com.odeyalo.sonata.authentication.support.validation.UserRegistrationInfoValidator;
+import com.odeyalo.sonata.authentication.support.validation.step.UserRegistrationInfoValidationStep;
+import com.odeyalo.sonata.authentication.testing.stubs.InvalidEmailDenyingUserRegistrationInfoValidationStepStub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The factory is used to create a new {@link UserRegistrationInfoValidator} for testing purposes.
+ */
+public class UserRegistrationInfoValidatorTestingFactory {
+
+    public static UserRegistrationInfoValidator createEmpty() {
+        return new DefaultChainUserRegistrationInfoValidator(new UserRegistrationInfoValidationStepContainer());
+    }
+
+    /**
+     * Create {@link DefaultChainUserRegistrationInfoValidator} with already pre-initialized {@link com.odeyalo.sonata.authentication.support.validation.step.ValidationStep}
+     *
+     * @return - DefaultChainUserRegistrationInfoValidator with registered ValidationSteps
+     */
+    public static UserRegistrationInfoValidator create() {
+        List<UserRegistrationInfoValidationStep> validators = new ArrayList<>();
+
+        validators.add(new InvalidEmailDenyingUserRegistrationInfoValidationStepStub());
+
+        return new DefaultChainUserRegistrationInfoValidator(new UserRegistrationInfoValidationStepContainer(validators));
+    }
+
+    public static DefaultChainUserRegistrationInfoValidator createChainedValidator() {
+        UserRegistrationInfoValidationStepRegistry registry = UserRegistrationInfoValidationStepRegistryTestingFactory.create();
+        return new DefaultChainUserRegistrationInfoValidator(registry);
+    }
+}

e453e793a98bdf403b7837feddf739d8664cf665
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-28 15:16:34 +0200

    Changed parent interfaces

--- src/main/java/com/odeyalo/sonata/authentication/support/validation/DefaultChainUserRegistrationInfoValidator.java
+++ src/main/java/com/odeyalo/sonata/authentication/support/validation/DefaultChainUserRegistrationInfoValidator.java
@@ -3,27 +3,20 @@ package com.odeyalo.sonata.authentication.support.validation;
 import com.odeyalo.sonata.authentication.common.ErrorDetails;
 import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
 import com.odeyalo.sonata.authentication.support.validation.step.UserRegistrationInfoValidationStep;
-import com.odeyalo.sonata.authentication.support.validation.step.ValidationStep;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 /**
  * Default {@link UserRegistrationInfoValidator} implementation, that uses {@link UserRegistrationInfoValidationStep} to validate the {@link UserRegistrationInfo}
  */
 @Component
-public class DefaultChainUserRegistrationInfoValidator implements UserRegistrationInfoValidator, UserRegistrationInfoValidationStepRegistry {
-    private final List<UserRegistrationInfoValidationStep> validators;
+public class DefaultChainUserRegistrationInfoValidator implements UserRegistrationInfoValidator {
+    private final UserRegistrationInfoValidationStepRegistry container;
 
-    public DefaultChainUserRegistrationInfoValidator() {
-        this.validators = new ArrayList<>();
-    }
-
-    public DefaultChainUserRegistrationInfoValidator(List<UserRegistrationInfoValidationStep> validators) {
-        this.validators = validators;
+    public DefaultChainUserRegistrationInfoValidator(UserRegistrationInfoValidationStepRegistry container) {
+        this.container = container;
     }
 
     @Override
@@ -43,33 +36,7 @@ public class DefaultChainUserRegistrationInfoValidator implements UserRegistrati
         return ValidationResult.success();
     }
 
-    @Override
-    public List<ValidationStep<UserRegistrationInfo>> getSteps() {
-        return null;
-    }
-
-    @Override
-    public void add(ValidationStep<UserRegistrationInfo> step) {
-
-    }
-
-    @Override
-    public void add(int index, ValidationStep<UserRegistrationInfo> step) {
-
-    }
-
-    @Override
-    public void remove(ValidationStep<UserRegistrationInfo> step) {
-
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void clear() {
-
+    public UserRegistrationInfoValidationStepRegistry getContainer() {
+        return container;
     }
 }

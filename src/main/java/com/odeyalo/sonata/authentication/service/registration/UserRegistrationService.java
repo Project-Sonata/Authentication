10164f963aa2bf06c6ab5b9dab025f0164c85fe8
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-01 21:26:14 +0200

    Register the user in the Sonata Application

---
+++ src/main/java/com/odeyalo/sonata/authentication/service/registration/UserRegistrationService.java
@@ -0,0 +1,12 @@
+package com.odeyalo.sonata.authentication.service.registration;
+
+import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
+
+public interface UserRegistrationService {
+    /**
+     * Register the user in the Sonata Application
+     * @param info - provided registration info by user
+     * @return - {@link RegistrationResult#success()} if user was registered, {@link RegistrationResult#failed(RegistrationResult.RequiredAction)} otherwise
+     */
+    RegistrationResult registerUser(UserRegistrationInfo info);
+}

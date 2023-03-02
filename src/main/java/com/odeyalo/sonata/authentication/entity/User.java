346c4f680544f42ccf090ce4e97c9d973f160232
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-02 15:00:18 +0200

    Added password and naturalId fields, added annotations with column settings

--- src/main/java/com/odeyalo/sonata/authentication/entity/User.java
+++ src/main/java/com/odeyalo/sonata/authentication/entity/User.java
@@ -1,17 +1,40 @@
 package com.odeyalo.sonata.authentication.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
 /**
  * Entity class to store the data about end-user
  */
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @Column(name = "password", nullable = false, length = 3000)
+    private String password;
+    @Column(name = "is_activated", nullable = false)
     private boolean active;
+
+    @NaturalId
+    private String naturalId;
+
+    public User(Long id, String email, String password, boolean active) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.active = active;
+    }
 }

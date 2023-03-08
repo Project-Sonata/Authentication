eb9365063e85f0f83c97685ca8a69a2a843ae984
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-08 21:34:57 +0200

    Added several methods

--- src/main/java/com/odeyalo/sonata/authentication/service/registration/RegistrationResult.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/registration/RegistrationResult.java
@@ -1,14 +1,28 @@
 package com.odeyalo.sonata.authentication.service.registration;
 
+import com.odeyalo.sonata.authentication.common.ErrorDetails;
+
 /**
  * Immutable class that stores info about registration
  * @param success - true if registration was success, false otherwise
  * @param action - required action to activate the user
+ * @param errorDetails - details about error that was occurred, can be null if everything is okay.
+ *
+ * Using of constructor is not recommended and static methods should be used to create a new RegistrationResult
  */
-public record RegistrationResult(boolean success, RequiredAction action) {
+
+public record RegistrationResult(boolean success, RequiredAction action, ErrorDetails errorDetails) {
+
+    public RegistrationResult(boolean success, RequiredAction action) {
+        this(success, action, null);
+    }
 
     public static RegistrationResult of(boolean success, RequiredAction action) {
         return new RegistrationResult(success, action);
+    }
+
+    public static RegistrationResult of(boolean success, RequiredAction action, ErrorDetails errorDetails) {
+        return new RegistrationResult(success, action, errorDetails);
     }
 
     public static RegistrationResult success(RequiredAction action) {
@@ -16,11 +30,19 @@ public record RegistrationResult(boolean success, RequiredAction action) {
     }
 
     public static RegistrationResult failed(RequiredAction action) {
-        return of(true, action);
+        return of(false, action);
+    }
+
+    public static RegistrationResult failed(RequiredAction action, ErrorDetails errorDetails) {
+        return of(false, action, errorDetails);
     }
 
     public record RequiredAction(String actionName) {
         public static final RequiredAction DO_NOTHING = new RequiredAction("DO_NOTHING");
         public static final RequiredAction CONFIRM_EMAIL = new RequiredAction("CONFIRM_EMAIL");
+
+        public static RequiredAction of(String actionName) {
+            return new RequiredAction(actionName);
+        }
     }
 }

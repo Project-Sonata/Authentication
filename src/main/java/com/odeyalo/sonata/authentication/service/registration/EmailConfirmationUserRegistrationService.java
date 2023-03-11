9c9b38f00a6176a007c56c1acd9119e0f61cccbb
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-11 20:39:10 +0200

    Added password encoder and mail sending

--- src/main/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationService.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationService.java
@@ -1,28 +1,70 @@
 package com.odeyalo.sonata.authentication.service.registration;
 
+import com.odeyalo.sonata.authentication.common.ErrorDetails;
 import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
 import com.odeyalo.sonata.authentication.entity.User;
 import com.odeyalo.sonata.authentication.repository.UserRepository;
+import com.odeyalo.sonata.authentication.service.confirmation.EmailConfirmationCodeGeneratorSender;
+import com.odeyalo.sonata.authentication.service.confirmation.EmailReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailConfirmationUserRegistrationService implements UserRegistrationService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailConfirmationCodeGeneratorSender confirmationCodeGeneratorSender;
+    private final Logger logger = LoggerFactory.getLogger(EmailConfirmationUserRegistrationService.class);
 
-    public EmailConfirmationUserRegistrationService(UserRepository userRepository) {
+    @Autowired
+    public EmailConfirmationUserRegistrationService(UserRepository userRepository,
+                                                    EmailConfirmationCodeGeneratorSender confirmationCodeGeneratorSender,
+                                                    PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.confirmationCodeGeneratorSender = confirmationCodeGeneratorSender;
     }
 
     @Override
     public RegistrationResult registerUser(UserRegistrationInfo info) {
-        User user = User
+        User user = createUser(info);
+        RegistrationResult result = save(user);
+        if (result.success()) {
+            sendConfirmationCode(info);
+        }
+        return result;
+    }
+
+    private void sendConfirmationCode(UserRegistrationInfo info) {
+        confirmationCodeGeneratorSender.generateAndSend(EmailReceiver.of(info.getEmail()));
+    }
+
+    /**
+     * Save the user in repository
+     * @param user - user to save
+     * @return - {@link RegistrationResult#success()})} if everything is okay, {@link RegistrationResult#failed(RegistrationResult.RequiredAction, ErrorDetails)}) otherwise
+     */
+    private RegistrationResult save(User user) {
+        try {
+            this.userRepository.save(user);
+            return RegistrationResult.success(RegistrationResult.RequiredAction.CONFIRM_EMAIL);
+        } catch (Exception e) {
+            this.logger.error("The error occurred during user saving.", e);
+            // We already validate the UserRegistrationInfo in the UserRegistrationManager and if error occurs we don't know what type of the error, so we return SERVER_ERROR
+            return RegistrationResult.failed(RegistrationResult.RequiredAction.DO_NOTHING, ErrorDetails.SERVER_ERROR);
+        }
+    }
+
+    private User createUser(UserRegistrationInfo info) {
+        String password = passwordEncoder.encode(info.getPassword());
+        return User
                 .builder()
                 .email(info.getEmail())
-                //todo
-                .password("[ENCODED]")
+                .password(password)
                 .active(false)
                 .build();
-        this.userRepository.save(user);
-        return new RegistrationResult(true, RegistrationResult.RequiredAction.CONFIRM_EMAIL);
     }
 }
--- src/test/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationServiceTest.java
+++ src/test/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationServiceTest.java
@@ -1,29 +1,44 @@
 package com.odeyalo.sonata.authentication.service.registration;
 
+import com.odeyalo.sonata.authentication.common.ErrorDetails;
 import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
 import com.odeyalo.sonata.authentication.entity.User;
-import com.odeyalo.sonata.authentication.repository.InMemoryUserRepository;
+import com.odeyalo.sonata.authentication.repository.UserRepository;
 import com.odeyalo.sonata.authentication.testing.factory.UserRegistrationServiceTestingFactory;
+import com.odeyalo.sonata.authentication.testing.spy.DelegatingEmailConfirmationCodeGeneratorSenderSpy;
+import com.odeyalo.sonata.authentication.testing.spy.EmptyEmailConfirmationCodeGeneratorSenderSpy;
+import com.odeyalo.sonata.authentication.testing.stubs.ThrowableUserRepositoryStub;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+// SavingConfirmationCodeGenerator - generate and save the confirmation code in some kind of storage
+// EmailConfirmationManager - manager that create, validate and delete confirmation codes and activate the user if code is valid
+// EmailConfirmationCodeGeneratorSender - Facade service to  generate and send confirmation code to email
+// ConfirmationCodeGenerator - generate the confirmation code
+// MailSender - sender that used to send the message to the email
 class EmailConfirmationUserRegistrationServiceTest {
 
     @Test
     @DisplayName("Register the user with valid info and expect success as result")
     void registerUserWithValidInfo_andExpectSuccess() {
         // Given
-        InMemoryUserRepository spy = new InMemoryUserRepository();
         String email = "odeyalo@gmail.com";
         String password = "password123";
 
-        EmailConfirmationUserRegistrationService userRegistrationService = UserRegistrationServiceTestingFactory
-                .createDefaultService()
-                .overrideUserRepository(spy)
+        UserRegistrationServiceTestingFactory.DefaultUserRegistrationServiceBuilder builder = UserRegistrationServiceTestingFactory
+                .createDefaultService();
+
+        EmptyEmailConfirmationCodeGeneratorSenderSpy senderSpy = new EmptyEmailConfirmationCodeGeneratorSenderSpy();
+
+        UserRepository userRepositorySpy = builder.getUserRepository();
+        PasswordEncoder passwordEncoderSpy = builder.getPasswordEncoder();
+
+        EmailConfirmationUserRegistrationService userRegistrationService = builder
+                .overrideEmailConfirmationCodeGeneratorSender(senderSpy)
                 .build();
 
         UserRegistrationInfo info = new UserRegistrationInfo(email, password,
@@ -34,13 +49,36 @@ class EmailConfirmationUserRegistrationServiceTest {
         assertTrue(result.success(), "If the user data is valid, then user should be registered and 'success' field should be true");
         assertEquals(RegistrationResult.RequiredAction.CONFIRM_EMAIL, result.action(),
                 "If the user enter email and use default registration form, then RequiredAction.CONFIRM_EMAIL should be returned");
-        User actualUser = spy.findUserByEmail(email);
+        User actualUser = userRepositorySpy.findUserByEmail(email);
 
         assertNotNull(actualUser, "If the user has been registered, then user should be added to DB or other type of storage");
         assertFalse(actualUser.isActive(), "If required action is CONFIRM_EMAIL, then user should be activated only after email confirmation");
-        assertNotNull(actualUser.getEmail(), "Email must be saved!");
         assertEquals(email, actualUser.getEmail(), "Email must be saved!");
-        assertNotNull(actualUser.getPassword(), "Password must be encoded and saved!");
-        assertNotEquals(password, actualUser.getPassword(), "Password must be encoded and saved!");
+        assertTrue(passwordEncoderSpy.matches(password, actualUser.getPassword()), "Raw password must be match encoded!");
+
+        assertTrue(senderSpy.wasSent(), "EmailConfirmationUserRegistrationService must send the confirmation code to the email!");
+    }
+
+    @Test
+    @DisplayName("Register user with occurred error and expect RegistrationResult.failed()")
+    void registerUserWithException_andExpectFailed() {
+        String email = "odeyalo@gmail.com";
+        String password = "password123";
+
+        // Given
+        UserRepository userRepository = new ThrowableUserRepositoryStub();
+        EmailConfirmationUserRegistrationService registrationService = UserRegistrationServiceTestingFactory.createDefaultService()
+                .overrideUserRepository(userRepository)
+                .build();
+
+
+        UserRegistrationInfo info = new UserRegistrationInfo(email, password,
+                "MALE", LocalDate.of(2000, 12, 12), false);
+        // When
+        RegistrationResult registrationResult = registrationService.registerUser(info);
+        // Then
+        assertFalse(registrationResult.success(), "If any error was occurred during saving, then false must be returned!");
+        assertEquals(RegistrationResult.RequiredAction.DO_NOTHING, registrationResult.action(), "RequiredAction.DO_NOTHING must be returned!");
+        assertEquals(ErrorDetails.SERVER_ERROR, registrationResult.errorDetails(), "ErrorDetails.SERVER_ERROR must be returned if the saving operation cannot be performed because repository throws an exception!");
     }
 }

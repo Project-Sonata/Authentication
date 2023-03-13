f1e596c4dcc2a6a3821188d7838b88eb43f00c5d
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-13 18:43:30 +0200

    Interface that manages lifecycle of the confirmation code

---
+++ src/main/java/com/odeyalo/sonata/authentication/service/confirmation/support/ConfirmationCodeLifecycleHandler.java
@@ -0,0 +1,38 @@
+package com.odeyalo.sonata.authentication.service.confirmation.support;
+
+import com.odeyalo.sonata.authentication.entity.ConfirmationCode;
+
+/**
+ * Interface that manages lifecycle of the confirmation code
+ */
+public interface ConfirmationCodeLifecycleHandler {
+    /**
+     * Provide the info about current confirmation code lifecycle stage
+     * @param code - confirmation code entity
+     * @return - lifecycle stage of the provided confirmation code
+     */
+    ConfirmationCode.LifecycleStage getLifecycleStage(ConfirmationCode code);
+
+    /**
+     * Same as {@link #getLifecycleStage(ConfirmationCode)} but do it by code value.
+     * @param codeValue - confirmation code value
+     * @return - lifecycle stage of the provided confirmation code
+     */
+    ConfirmationCode.LifecycleStage getLifecycleStage(String codeValue);
+
+    /**
+     * Change the confirmation code lifecycle stage to provided
+     * @param code - confirmation code to change the lifecycle stage
+     * @param stage - required stage to set
+     * @return - updated {@link ConfirmationCode}
+     */
+    ConfirmationCode changeLifecycleStage(ConfirmationCode code, ConfirmationCode.LifecycleStage stage);
+
+    /**
+     * Same as {@link #changeLifecycleStage(ConfirmationCode, ConfirmationCode.LifecycleStage)} but do it with confirmation code value.
+     * @param codeValue - confirmation code value to change the lifecycle stage
+     * @param stage - required stage to set
+     * @return - updated {@link ConfirmationCode}
+     */
+    ConfirmationCode changeLifecycleStage(String codeValue,  ConfirmationCode.LifecycleStage stage);
+}

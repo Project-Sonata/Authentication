746a8d2b3cb707feb92362b92273e88227222255
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-15 22:34:55 +0200

    Changed generateAndSend() method contract, now it can throw MessageSendingFailedException

--- src/main/java/com/odeyalo/sonata/authentication/service/confirmation/EmailConfirmationCodeGeneratorSender.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/confirmation/EmailConfirmationCodeGeneratorSender.java
@@ -1,5 +1,7 @@
 package com.odeyalo.sonata.authentication.service.confirmation;
 
+import com.odeyalo.sonata.authentication.exceptions.MessageSendingFailedException;
+
 /**
  * Simple facade interface to generate a confirmation code and send it to specific email.
  */
@@ -8,6 +10,6 @@ public interface EmailConfirmationCodeGeneratorSender {
      * Generate a confirmation code and send it to {@link EmailReceiver}
      * @param receiver - receiver that will receive the confirmation code
      */
-    void generateAndSend(EmailReceiver receiver);
+    void generateAndSend(EmailReceiver receiver) throws MessageSendingFailedException;
 
 }

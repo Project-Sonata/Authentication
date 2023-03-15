ed1ffb3e32b0007717865d234e3af25daf81c119
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-15 22:33:23 +0200

    Added builder

--- src/test/java/com/odeyalo/sonata/authentication/testing/factory/ConfirmationCodeEmailMessageCreatorTestingFactory.java
+++ src/test/java/com/odeyalo/sonata/authentication/testing/factory/ConfirmationCodeEmailMessageCreatorTestingFactory.java
@@ -15,4 +15,35 @@ public class ConfirmationCodeEmailMessageCreatorTestingFactory {
     public static PlainTextConfirmationCodeEmailMessageCreator plainText() {
         return new PlainTextConfirmationCodeEmailMessageCreator();
     }
+
+    public static PlainTextConfirmationCodeEmailMessageCreatorBuilder plainTextBuilder() {
+        return new PlainTextConfirmationCodeEmailMessageCreatorBuilder();
+    }
+
+    public static class PlainTextConfirmationCodeEmailMessageCreatorBuilder {
+        private String subject;
+        private String bodyFormat;
+
+        public PlainTextConfirmationCodeEmailMessageCreatorBuilder overrideSubject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public PlainTextConfirmationCodeEmailMessageCreatorBuilder overrideBodyFormat(String bodyFormat) {
+            this.bodyFormat = bodyFormat;
+            return this;
+        }
+
+        public PlainTextConfirmationCodeEmailMessageCreator build() {
+            PlainTextConfirmationCodeEmailMessageCreator creator = new PlainTextConfirmationCodeEmailMessageCreator();
+            if (!subject.isEmpty()) {
+                creator.overrideSubject(subject);
+            }
+            if (!bodyFormat.isEmpty()) {
+                creator.overrideBodyFormat(bodyFormat);
+            }
+            return creator;
+        }
+    }
+
 }

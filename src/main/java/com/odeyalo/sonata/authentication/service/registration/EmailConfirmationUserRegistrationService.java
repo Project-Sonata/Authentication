44265f7567492351aeb8137bc614e4b38bad29ee
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-16 14:50:53 +0200

    Added exception handling and different RegistrationResult based on exception

--- src/main/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationService.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/registration/EmailConfirmationUserRegistrationService.java
@@ -3,14 +3,18 @@ package com.odeyalo.sonata.authentication.service.registration;
 import com.odeyalo.sonata.authentication.common.ErrorDetails;
 import com.odeyalo.sonata.authentication.dto.request.UserRegistrationInfo;
 import com.odeyalo.sonata.authentication.entity.User;
+import com.odeyalo.sonata.authentication.exceptions.MessageSendingFailedException;
 import com.odeyalo.sonata.authentication.repository.UserRepository;
 import com.odeyalo.sonata.authentication.service.confirmation.EmailConfirmationCodeGeneratorSender;
 import com.odeyalo.sonata.authentication.service.confirmation.EmailReceiver;
+import org.apache.commons.lang3.BooleanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 @Service
 public class EmailConfirmationUserRegistrationService implements UserRegistrationService {
@@ -31,14 +35,30 @@ public class EmailConfirmationUserRegistrationService implements UserRegistratio
     @Override
     public RegistrationResult registerUser(UserRegistrationInfo info) {
         User user = createUser(info);
-        RegistrationResult result = save(user);
-        if (result.success()) {
-            sendConfirmationCode(info);
+        ResultWrapper wrapper = saveUser(user);
+
+        if (wrapper.isFailed()) {
+            this.logger.warn("Failed to register the user: {}", wrapper);
+            return wrapper.result();
         }
-        return result;
+
+        return tryToSendOr(info, wrapper, (resultWrapper) -> {
+            userRepository.deleteById(resultWrapper.user().getId());
+        });
     }
 
-    private void sendConfirmationCode(UserRegistrationInfo info) {
+    private RegistrationResult tryToSendOr(UserRegistrationInfo info, ResultWrapper wrapper, Consumer<ResultWrapper> onError) {
+        try {
+            sendConfirmationCode(info);
+            return wrapper.result();
+        } catch (MessageSendingFailedException ex) {
+            this.logger.error("Failed to send the confirmation code to: {}", info.getEmail());
+            onError.accept(wrapper);
+            return RegistrationResult.failed(RegistrationResult.RequiredAction.DO_NOTHING, ErrorDetails.SERVER_ERROR);
+        }
+    }
+
+    private void sendConfirmationCode(UserRegistrationInfo info) throws MessageSendingFailedException {
         confirmationCodeGeneratorSender.generateAndSend(EmailReceiver.of(info.getEmail()));
     }
 
@@ -47,14 +67,16 @@ public class EmailConfirmationUserRegistrationService implements UserRegistratio
      * @param user - user to save
      * @return - {@link RegistrationResult#success()})} if everything is okay, {@link RegistrationResult#failed(RegistrationResult.RequiredAction, ErrorDetails)}) otherwise
      */
-    private RegistrationResult save(User user) {
+    private ResultWrapper saveUser(User user) {
         try {
-            this.userRepository.save(user);
-            return RegistrationResult.success(RegistrationResult.RequiredAction.CONFIRM_EMAIL);
+            User savedUser = this.userRepository.save(user);
+            RegistrationResult result = RegistrationResult.success(RegistrationResult.RequiredAction.CONFIRM_EMAIL);
+            return ResultWrapper.of(savedUser, result);
         } catch (Exception e) {
             this.logger.error("The error occurred during user saving.", e);
             // We already validate the UserRegistrationInfo in the UserRegistrationManager and if error occurs we don't know what type of the error, so we return SERVER_ERROR
-            return RegistrationResult.failed(RegistrationResult.RequiredAction.DO_NOTHING, ErrorDetails.SERVER_ERROR);
+            RegistrationResult result = RegistrationResult.failed(RegistrationResult.RequiredAction.DO_NOTHING, ErrorDetails.SERVER_ERROR);
+            return ResultWrapper.of(null, result);
         }
     }
 
@@ -66,5 +88,26 @@ public class EmailConfirmationUserRegistrationService implements UserRegistratio
                 .password(password)
                 .active(false)
                 .build();
+    }
+
+    /**
+     * Simple wrapper to wrap user and registration result in the one class
+     * If result is failed, then user must be null
+     * @param user   - user that was saved
+     * @param result - result of the registration
+     */
+    private record ResultWrapper(User user, RegistrationResult result) {
+
+        public boolean isFailed() {
+            return BooleanUtils.isFalse(result.success());
+        }
+
+        public boolean isSuccess() {
+            return result.success();
+        }
+
+        public static ResultWrapper of(User user, RegistrationResult result) {
+            return new ResultWrapper(user, result);
+        }
     }
 }

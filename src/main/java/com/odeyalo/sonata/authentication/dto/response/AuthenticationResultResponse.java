5a8ca24fb0db1aba86bef265c7d00a82ece6700f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-26 18:47:53 +0300

    Added Error details

--- src/main/java/com/odeyalo/sonata/authentication/dto/response/AuthenticationResultResponse.java
+++ src/main/java/com/odeyalo/sonata/authentication/dto/response/AuthenticationResultResponse.java
@@ -1,7 +1,9 @@
 package com.odeyalo.sonata.authentication.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.odeyalo.sonata.authentication.common.AuthenticationResult;
+import com.odeyalo.sonata.authentication.common.ErrorDetails;
 import com.odeyalo.sonata.authentication.dto.UserInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,17 +12,25 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthenticationResultResponse {
     private boolean success;
     @JsonProperty("user_info")
     private UserInfo userInfo;
+    @JsonProperty("error_details")
+    private ErrorDetails errorDetails;
+
+    public AuthenticationResultResponse(boolean success, UserInfo userInfo) {
+        this.success = success;
+        this.userInfo = userInfo;
+    }
 
     public static AuthenticationResultResponse from(AuthenticationResult result) {
         if (result.isSuccess()) {
             UserInfo userInfo = UserInfo.from(result.getUser());
             return success(userInfo);
         }
-        return failed();
+        return failed(result.getErrorDetails());
     }
 
 
@@ -28,7 +38,7 @@ public class AuthenticationResultResponse {
         return new AuthenticationResultResponse(true, userInfo);
     }
 
-    public static AuthenticationResultResponse failed() {
-        return new AuthenticationResultResponse(false, null);
+    public static AuthenticationResultResponse failed(ErrorDetails details) {
+        return new AuthenticationResultResponse(false, null, details);
     }
 }

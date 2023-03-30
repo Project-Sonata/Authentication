2383b7276d264d722c0f7ffe59a433d00a888059
Odeyalo <theprogrammerinfuture@gmail.com>
2023-03-30 18:13:43 +0300

    Added Type of the result. Type is a more detailed information about authentication status

--- src/main/java/com/odeyalo/sonata/authentication/common/AuthenticationResult.java
+++ src/main/java/com/odeyalo/sonata/authentication/common/AuthenticationResult.java
@@ -3,6 +3,7 @@ package com.odeyalo.sonata.authentication.common;
 import com.odeyalo.sonata.authentication.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
@@ -16,19 +17,35 @@ public class AuthenticationResult {
     private User user;
     // Used if the application cannot perform authentication
     private ErrorDetails errorDetails;
-
+    private Type type;
 
     public AuthenticationResult(boolean success, User user) {
         this.success = success;
         this.user = user;
     }
 
-    public static AuthenticationResult success(User user) {
-        return new AuthenticationResult(true, user);
+    public static AuthenticationResult success(User user, Type type) {
+        return new AuthenticationResult(true, user, null, type);
     }
 
     public static AuthenticationResult failed(ErrorDetails details) {
-        return new AuthenticationResult(false, null, details);
+        return new AuthenticationResult(false, null, details, Type.FAILED);
+    }
+
+    /**
+     * More detailed information about authentication status
+     */
+    @Getter
+    public enum Type {
+        PENDING_MFA("pending_mfa"),
+        LOGIN_COMPLETED("login_completed"),
+        // If authentication cannot be performed
+        FAILED("failed");
+        private final String name;
+
+        Type(String name) {
+            this.name = name;
+        }
     }
 
     public static final class PossibleErrors {

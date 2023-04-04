e4339742353c26aa728fd0dd5313eead587c18c5
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-04 18:27:00 +0300

    Added DataRequestAssociationService to associate the user entity with login session if login isn't complete

--- src/main/java/com/odeyalo/sonata/authentication/controller/AuthController.java
+++ src/main/java/com/odeyalo/sonata/authentication/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.authentication.controller;
 
 import com.odeyalo.sonata.authentication.common.LoginCredentials;
 import com.odeyalo.sonata.authentication.common.AuthenticationResult;
+import com.odeyalo.sonata.authentication.controller.support.DataRequestAssociationService;
 import com.odeyalo.sonata.authentication.dto.UserInfo;
 import com.odeyalo.sonata.authentication.dto.error.ApiErrorDetailsInfo;
 import com.odeyalo.sonata.authentication.dto.request.ConfirmationCodeRequestDto;
@@ -14,6 +15,8 @@ import com.odeyalo.sonata.authentication.service.confirmation.support.Confirmati
 import com.odeyalo.sonata.authentication.service.login.AuthenticationManager;
 import com.odeyalo.sonata.authentication.service.registration.RegistrationResult;
 import com.odeyalo.sonata.authentication.service.registration.UserRegistrationManager;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -25,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.odeyalo.sonata.authentication.common.AuthenticationResult.Type.LOGIN_COMPLETED;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -34,15 +38,18 @@ public class AuthController {
     public static final String INVALID_CONFIRMATION_CODE_MESSAGE = "The code is invalid";
     public static final String CONFIRMATION_URL_REL = "confirmation_url";
     public static final String CONFIRMATION_CODE_WAS_SENT_MESSAGE = "We sent confirmation letter to your email. Check it out";
+    public static final String LOGIN_ASSOCIATED_USER_KEY = "login_associated_user_key";
     private final UserRegistrationManager userRegistrationManager;
     private final EmailConfirmationManager emailConfirmationManager;
     private final AuthenticationManager authenticationManager;
+    private final DataRequestAssociationService dataRequestAssociationService;
 
     @Autowired
-    public AuthController(UserRegistrationManager userRegistrationManager, EmailConfirmationManager emailConfirmationManager, AuthenticationManager authenticationManager) {
+    public AuthController(UserRegistrationManager userRegistrationManager, EmailConfirmationManager emailConfirmationManager, AuthenticationManager authenticationManager, DataRequestAssociationService dataRequestAssociationService) {
         this.userRegistrationManager = userRegistrationManager;
         this.emailConfirmationManager = emailConfirmationManager;
         this.authenticationManager = authenticationManager;
+        this.dataRequestAssociationService = dataRequestAssociationService;
     }
 
     @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -72,13 +79,23 @@ public class AuthController {
     }
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AuthenticationResultResponse> loginUser(@RequestBody LoginCredentials credentials) {
+    public ResponseEntity<AuthenticationResultResponse> loginUser(@RequestBody LoginCredentials credentials,
+                                                                  HttpServletRequest request,
+                                                                  HttpServletResponse response) {
         AuthenticationResult result = authenticationManager.authenticate(credentials);
         AuthenticationResultResponse body = AuthenticationResultResponse.from(result);
+
+        associateIfNecessary(request, response, result);
 
         return ResponseEntity.status(body.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(body);
+    }
+
+    private void associateIfNecessary(HttpServletRequest request, HttpServletResponse response, AuthenticationResult result) {
+        if (result.getType() != LOGIN_COMPLETED) {
+            dataRequestAssociationService.associateData(LOGIN_ASSOCIATED_USER_KEY, result.getUser(), request, response);
+        }
     }
 
     private ResponseEntity<UserRegistrationConfirmationResponseDto> getSuccessResponse(UserRegistrationInfo info, UserRegistrationConfirmationResponseDto dto) {

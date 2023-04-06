5e657eacfaa09b81c5b948575255c2782f77f06f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-06 18:05:00 +0300

    Deleted lombok annotations

--- src/main/java/com/odeyalo/sonata/authentication/service/mfa/MfaConfirmationSubmission.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/mfa/MfaConfirmationSubmission.java
@@ -1,17 +1,13 @@
 package com.odeyalo.sonata.authentication.service.mfa;
 
 import com.odeyalo.sonata.authentication.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 
 /**
  * Immutable class to store the info about MFA submission
  * @param user - user that need to confirm his identity
  * @param content - content that was sent by user
  */
-@AllArgsConstructor
-@Data
 @Builder
 public record MfaConfirmationSubmission(User user, String content) {
 

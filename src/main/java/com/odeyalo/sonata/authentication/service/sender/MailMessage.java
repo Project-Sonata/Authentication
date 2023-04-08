50be4c9294a6cd0b575a0689f31d6f1566afe705
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-08 14:36:45 +0300

    Added EmailMessageTypeCode and null checks

--- src/main/java/com/odeyalo/sonata/authentication/service/sender/MailMessage.java
+++ src/main/java/com/odeyalo/sonata/authentication/service/sender/MailMessage.java
@@ -1,9 +1,11 @@
 package com.odeyalo.sonata.authentication.service.sender;
 
 import com.odeyalo.sonata.authentication.service.confirmation.EmailReceiver;
+import com.odeyalo.sonata.authentication.support.email.message.EmailMessageTypeCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 /**
  * Represent message that can be sent to email
@@ -12,7 +14,12 @@ import lombok.Data;
 @Builder
 @Data
 public class MailMessage {
+    @NonNull
+    protected final EmailMessageTypeCode messageType;
+    @NonNull
     protected final EmailReceiver receiver;
+    @NonNull
     protected final byte[] content;
+    @NonNull
     protected final String subject;
 }

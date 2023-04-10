82fef22f462469e97d7e2ea73b868220d56d6dca
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-10 15:44:40 +0300

    Added additional info

--- src/main/java/com/odeyalo/sonata/authentication/dto/error/ApiErrorDetailsInfo.java
+++ src/main/java/com/odeyalo/sonata/authentication/dto/error/ApiErrorDetailsInfo.java
@@ -8,6 +8,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,11 +21,24 @@ public class ApiErrorDetailsInfo implements Status {
     private String statusName;
     @JsonProperty("error")
     private ErrorDetails errorDetails;
+    @JsonProperty("info")
+    private Map<String, Object> additionalInfo = new HashMap<>();
+
+    public ApiErrorDetailsInfo(int statusCode, String statusName, ErrorDetails errorDetails) {
+        this.statusCode = statusCode;
+        this.statusName = statusName;
+        this.errorDetails = errorDetails;
+    }
 
     public ApiErrorDetailsInfo(HttpStatus status, ErrorDetails errorDetails) {
         this.statusCode = status.value();
         this.statusName = status.getReasonPhrase();
         this.errorDetails = errorDetails;
+    }
+
+    public ApiErrorDetailsInfo addInfo(String key, Object value) {
+        additionalInfo.put(key, value);
+        return this;
     }
 
     @Override

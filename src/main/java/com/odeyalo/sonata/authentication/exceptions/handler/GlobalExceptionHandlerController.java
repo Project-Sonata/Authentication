1979e495d859c60e263a97f4e0d479779d0090c2
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-10 15:07:05 +0300

    Added UnsupportedMfaMethodException handling

--- src/main/java/com/odeyalo/sonata/authentication/exceptions/handler/GlobalExceptionHandlerController.java
+++ src/main/java/com/odeyalo/sonata/authentication/exceptions/handler/GlobalExceptionHandlerController.java
@@ -1,8 +1,10 @@
 package com.odeyalo.sonata.authentication.exceptions.handler;
 
 import com.odeyalo.sonata.authentication.common.ErrorDetails;
+import com.odeyalo.sonata.authentication.dto.ErrorAdditionalInfoKeys;
 import com.odeyalo.sonata.authentication.dto.error.ApiErrorDetailsInfo;
 import com.odeyalo.sonata.authentication.exceptions.MalformedLoginSessionException;
+import com.odeyalo.sonata.authentication.exceptions.UnsupportedMfaMethodException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +16,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
+    public static final ErrorDetails MFA_METHOD_DOES_NOT_SUPPORTED = ErrorDetails.of("invalid_mfa_method",
+            "This method does not supported", "Use valid MFA method");
+
+    public static final ErrorDetails INVALID_SESSION = ErrorDetails.of("invalid_session",
+            "Login cannot be performed since login session is malformed", "Try to login using credentials.");
 
     @ExceptionHandler(MalformedLoginSessionException.class)
     public ResponseEntity<?> handleMalformedLoginSessionException(MalformedLoginSessionException ex) {
-        ErrorDetails invalidSession = ErrorDetails.of(
-                "invalid_session",
-                "Login cannot be performed since login session is malformed",
-                "Try to login using credentials.");
-        ApiErrorDetailsInfo info = new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, invalidSession);
+        ApiErrorDetailsInfo info = new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, INVALID_SESSION);
         return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(info);
+    }
+
+    @ExceptionHandler(UnsupportedMfaMethodException.class)
+    public ResponseEntity<?> handleUnsupportedMfaMethodException(UnsupportedMfaMethodException ex) {
+        ApiErrorDetailsInfo details = new ApiErrorDetailsInfo(HttpStatus.BAD_REQUEST, MFA_METHOD_DOES_NOT_SUPPORTED)
+                        .addInfo(ErrorAdditionalInfoKeys.UNSUPPORTED_MFA_METHOD, ex.getMethod());
+
+        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(details);
     }
 }

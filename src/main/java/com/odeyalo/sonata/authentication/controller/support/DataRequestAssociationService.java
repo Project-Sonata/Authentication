79980438e9766a7782ecc7032d91a05db2f7afdc
Odeyalo <theprogrammerinfuture@gmail.com>
2023-04-10 15:05:45 +0300

    Changed return type

--- src/main/java/com/odeyalo/sonata/authentication/controller/support/DataRequestAssociationService.java
+++ src/main/java/com/odeyalo/sonata/authentication/controller/support/DataRequestAssociationService.java
@@ -10,12 +10,35 @@ public interface DataRequestAssociationService {
     /**
      * Associate the data with request to access it lately
      * Note: value can be set saved inside the HttpSession, but it's not necessary.
-     * @param key - key that can be used to access value
-     * @param value - data to save
-     * @param request - current request
+     *
+     * @param key      - key that can be used to access value
+     * @param value    - data to save
+     * @param request  - current request
      * @param response - response to associate data to.
+     * @return - associated key/value pair with this response that was added to it.
      */
-    void associateData(String key, Object value, HttpServletRequest request, HttpServletResponse response);
+    AssociatedWith associateData(String key, Object value, HttpServletRequest request, HttpServletResponse response);
 
     Object get(HttpServletRequest request, String key);
+
+    /**
+     * Record to store the name and value of the saved data
+     * @param store - type of the storage that used to save the name and the value
+     * @param name - key of the saved header or cookie
+     * @param value - value of the saved header or cookie
+     */
+    record AssociatedWith(AssociatedWith.Store store, String name, String value) {
+
+        public static AssociatedWith headerStore(String name, String value) {
+            return new AssociatedWith(Store.HEADER, name, value);
+        }
+
+        public static AssociatedWith cookieStore(String name, String value) {
+            return new AssociatedWith(Store.COOKIE, name, value);
+        }
+
+        public enum Store {
+            HEADER, COOKIE
+        }
+    }
 }
--- src/main/java/com/odeyalo/sonata/authentication/controller/support/HttpSessionDataRequestAssociationService.java
+++ src/main/java/com/odeyalo/sonata/authentication/controller/support/HttpSessionDataRequestAssociationService.java
@@ -1,9 +1,15 @@
 package com.odeyalo.sonata.authentication.controller.support;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 /**
  * Associate the data with HttpSession
@@ -12,11 +18,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HttpSessionDataRequestAssociationService implements DataRequestAssociationService {
+    private final String cookieName;
+    private final Logger logger = LoggerFactory.getLogger(HttpSessionDataRequestAssociationService.class);
+
+    public HttpSessionDataRequestAssociationService() {
+        this.cookieName = "JSESSIONID";
+    }
+
+    @Autowired
+    public HttpSessionDataRequestAssociationService(@Value("${server.servlet.session.cookie.name:JSESSIONID}") String cookieName) {
+        Assert.notNull(cookieName, "Cookie name must be set!");
+        this.cookieName = cookieName;
+    }
+
+    @PostConstruct
+    void setup() {
+        logger.info("Service was successfully bootstrapped, using this session cookie name: {}", cookieName);
+    }
 
     @Override
-    public void associateData(String key, Object value, HttpServletRequest request, HttpServletResponse response) {
+    public AssociatedWith associateData(String key, Object value, HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         session.setAttribute(key, value);
+        AssociatedWith store = AssociatedWith.cookieStore(cookieName, session.getId());
+        logger.debug("Saved the data in session! Associated request with: {}", store);
+        return store;
     }
 
     @Override
